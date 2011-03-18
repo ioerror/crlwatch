f941c2daa49819f90b4a6679e698eeb91435eeac
Jacob Appelbaum <jacob@appelbaum.net>
2011-03-18 01:47:05 -0700

    Parse certs that are DER encoded or whatever OpenSSL guesses (PEM)

--- src/parse-raw-crl-to-text.sh
+++ src/parse-raw-crl-to-text.sh
@@ -2,5 +2,9 @@
 #
 # This parses a CRL into human readable text
 #
-openssl crl -inform DER -in $1 -text -fingerprint -issuer -fingerprint -hash \
+openssl crl -in $1 -inform DER -text -fingerprint -issuer -fingerprint -hash \
             -crlnumber -noout -lastupdate -nextupdate
+if [ $? == 1 ]; then
+  openssl crl -in $1  -text -fingerprint -issuer -fingerprint -hash \
+              -crlnumber -noout -lastupdate -nextupdate
+fi
