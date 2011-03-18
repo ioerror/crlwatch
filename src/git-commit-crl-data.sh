3f5128c7e5575203d566661f5a6fdb91a3b69645
Jacob Appelbaum <jacob@appelbaum.net>
2011-03-18 03:11:07 -0700

    change parser to something more modular

--- src/git-commit-crl-data.sh
+++ src/git-commit-crl-data.sh
@@ -9,10 +9,7 @@
 for crl in `cat $1`
 do
   hash="$(echo "$crl" | sha1sum|cut -f1 -d\  )";
-  openssl crl -inform DER -in $hash.crl -text -fingerprint \
-               -issuer -fingerprint -hash \
-               -crlnumber -noout -lastupdate -nextupdate \
-               -out ../crl-parsed/$hash.crl2txt
+  ../src/parse-raw-crl-to-text.sh $hash.crl > ../crl-parsed/$hash.crl2txt
   git add $hash.crl ../crl-parsed/$hash.crl2txt
   git commit -m "Update $crl into $hash.crl and parsed into ../crl-parsed/$hash.crl2txt" $hash.crl ../crl-parsed/$hash.crl2txt
 done
