3387b5744549368c92e981e78dc8a29c1f5537d0
Jacob Appelbaum <jacob@appelbaum.net>
2011-03-18 00:06:12 -0700

    Fetch crls into CWD from a parsed list

---
+++ src/fetch-crls.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+#
+# Fetch all CRLs listed in a given file
+#
+
+for crl in `cat $1`
+do
+ echo "Fetching $crl";
+ usewithtor wget -c --user-agent="Mozilla/5.0 (Windows; U; Windows NT 6.1; LANG; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3" $crl;
+ if [ $? != 0 ];
+ then
+   echo "Error fetching $crl" 1>&2;
+ fi
+done
