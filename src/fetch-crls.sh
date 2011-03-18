0cfcf1712f578bf8b35868daccf61663a82ae406
Jacob Appelbaum <jacob@appelbaum.net>
2011-03-18 00:28:01 -0700

    Fetch CRLs into a file system safe file name

--- src/fetch-crls.sh
+++ src/fetch-crls.sh
@@ -6,7 +6,8 @@
 for crl in `cat $1`
 do
  echo "Fetching $crl";
- usewithtor wget -c --user-agent="Mozilla/5.0 (Windows; U; Windows NT 6.1; LANG; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3" $crl;
+ usewithtor wget -c --user-agent="Mozilla/5.0 (Windows; U; Windows NT 6.1; LANG; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3" \
+            -O $(echo "$crl" | sha1sum|cut -f1 -d\  ).crl "$crl";
  if [ $? != 0 ];
  then
    echo "Error fetching $crl" 1>&2;
