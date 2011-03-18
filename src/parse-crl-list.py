9a415bb8af82745e289196a84650f5f5a6eab96e
Jacob Appelbaum <jacob@appelbaum.net>
2011-03-17 23:26:03 -0700

    parse all CRL output formats

--- src/parse-crl-list.py
+++ src/parse-crl-list.py
@@ -4,14 +4,21 @@
 #
 # Parse the raw output from the observatory into a list of CRLs suitable for
 # fetching over Tor.
-#
 
 import sys
 
 if len(sys.argv) < 2:
-   print """Pass file of distinct CRL distribution points."""
-   sys.exit(1)
+    print """Pass file of distinct CRL distribution points."""
+    sys.exit(1)
 
 for x in open(sys.argv[1], 'r').read().split():
-      if x.startswith('URI===http'):
-            print x[6:]
+    if x.startswith('URI===http'):
+        print x[6:]
+
+for x in open(sys.argv[1], 'r').read().split():
+    if x.startswith('http===//'):
+        print 'http://' + x[9:]
+
+for x in open(sys.argv[1], 'r').read().split():
+    if x.startswith('https===//'):
+        print 'https://' + x[10:]
