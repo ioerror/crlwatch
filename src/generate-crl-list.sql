cc543204e1dab701fbf940d228141858b2b1a625
Jacob Appelbaum <jacob@appelbaum.net>
2011-03-21 21:08:37 -0700

    Update CRL SQL fetching and CRL list

--- src/generate-crl-list.sql
+++ src/generate-crl-list.sql
@@ -1,2 +1,7 @@
-select distinct `X509v3 extensions:Netscape CA Revocation Url` from valid_certs into outfile '/tmp/ns_ca_crls.txt'
-select distinct `X509v3 extensions:Netscape Revocation Url` from valid_certs into outfile '/tmp/ns_crls.txt';
+select distinct `X509v3 extensions:Netscape CA Revocation Url` as CRLs from valid_certs
+UNION
+select distinct `X509v3 extensions:Netscape Revocation Url` from valid_certs
+UNION
+select distinct `X509v3 extensions:X509v3 CRL Distribution Points` from valid_certs
+UNION
+select distinct `X509v3 extensions:Authority Information Access:OCSP - URI` from valid_certs into outfile 'raw-crl-sql-output-list.txt';
